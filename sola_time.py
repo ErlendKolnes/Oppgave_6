b640b63466521ae79682780be1ed2a8eab5d1739
taksdal <ole.kristian.taksdal@gmail.com>
2024-10-01 13:35:47 +0200

    Update sola_time.py

--- sola_time.py
+++ sola_time.py
@@ -1,6 +1,6 @@
 # Funksjon for å dele opp strengen i komponenter
 import matplotlib.pyplot as plt
-
+import datetime as d
 
 def splitsolatime(data):
 
@@ -43,15 +43,33 @@ for linje in fil_solatime:
         s_date, s_time, s_trykk, s_temp = splitsolatime(linje)
         s_dato_l.append(s_date)
         s_tid_l.append(s_time)
-        s_trykk_l.append(float(s_trykk))
-        s_temp_l.append(float(s_temp))
+        s_trykk_l.append(s_trykk)
+        s_temp_l.append(s_temp)
+
+s_dato_tid_l = []
+
+for i in range (len(s_dato_l)):
+    s_dato_tid_l.append(s_dato_l[i] + " " + s_tid_l[i])
+
+
 
 print("Her er dato:", "\n", s_dato_l, "\n")
 print("Her er tid:", "\n", s_tid_l, "\n")
 print("Her er temp:", "\n", s_temp_l, "\n")
 print("Her er trykk:", "\n", s_trykk_l, "\n")
+print("Her er dato og tid sammen", "\n", s_dato_tid_l, "\n")
+
+plt.plot(s_dato_tid_l, s_temp_l)
+plt.plot(s_dato_tid_l, s_trykk_l)
+plt.xticks(["11.06.2021 01:00", "12.06.2021 00:00", "13.06.2021 00:00", "14.06.2021 00:00"])
+#plt.yticks([10, 100, 500, 1000, 2000])
 
 
-plt.plot(s_temp_l, s_trykk_l)
 
+print(len(s_dato_tid_l))
+print(len(s_temp_l))
+print(len(s_trykk_l))
+
+plt.show()
+#datetime_liste = d.datetime(s_dato_tid_l)
 fil_solatime.close()
