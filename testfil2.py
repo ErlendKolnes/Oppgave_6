aa9189c5af98f8c665d7030ecd07cb8afe3d4910
john66623 <john.h.bo@lyse.net>
2024-10-03 12:32:59 +0200

    Testfiler for å legge sammen to forskjellige koder

---
+++ testfil2.py
@@ -0,0 +1,12 @@
+#testfil2
+import datetime as d
+
+trykk2 = [1010, 1030, 1020]
+
+dt_liste = []
+
+for i in range(3):
+    dag = d.datetime(2021, 6, 11+i)
+    dt_liste.append(dag)
+
+print(dt_liste)
