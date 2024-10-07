040c0b22c47bfdcc9f272b15e73a22db8bf9ecbf
Sander Bolstad Stokkenes <sander.stokkenes@gmail.com>
2024-10-07 12:15:01 +0200

    ferdig

--- plotting.py
+++ plotting.py
@@ -13,7 +13,6 @@ plt.plot(gyldige_tidspunkter, snitt_temperaturer, label='Gjennomsnittstemperatur
 plt.plot(tid_hoy_lav_x, temp_hoy_lav_y, label='Høyeste og laveste temperatur', color='purple')
 plt.legend()
 
-#test_x = r_dates_times+rs_dtdato
 
 plt.subplot(2, 1, 2)
 plt.plot(s_dt_dato, s_trykk_l, label="Absolutt trykk MET", color="green")
@@ -21,8 +20,6 @@ plt.plot(r_dates_times, r_trykk_a, label="Absolutt trykk", color="blue")
 plt.plot(r_dates_times, r_trykk_b, label="Barometrisk trykk", color="orange")
 plt.legend()
 
-#print(r_dates_times)
 
-#print(s_dt_dato)
 
 plt.show()
