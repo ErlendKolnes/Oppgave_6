7844c4d69b682b29121d775585d2ad4972cbec8a
ErlendKolnes <erlend.kolnes@gmail.com>
2024-11-11 19:15:44 +0100

    Oppgave e
    Opgave e, ferdig håper jeg:)

--- gjennomsnitt.py
+++ gjennomsnitt.py
@@ -4,9 +4,8 @@ tidspunkter forskjellen mellom de to seriene er lavest og høyest. Dere trenger 
 sammenlikne de linjene i hver fil der tidspunktene er like (for hver dag og time i den ene
 fila, finn tilsvarende dag og time med 0 minutter i den andre fila)'''
 
-import datetime as d
+#Last ned pandas med å skrive pip: install pandas i terminalen
 import pandas as pd
-
 
 from sola_time import *
 from rune_time import *
@@ -22,6 +21,7 @@ r_filtrert_dates_times = [r_dates_times[i] for i in r_filtrer_verdier]
 r_filtrert_temps = [r_temps[i] for i in r_filtrer_verdier]
 r_filtrert_trykk_a = [r_trykk_a[i] for i in r_filtrer_verdier]
 
+#Filtrerer ut tidspunktene som har 00 minutter, feilsøking
 '''
 if __name__ == "__main__":
     print("Antall datoer: ", len(r_filtrert_dates_times))
@@ -50,26 +50,63 @@ met_inst_def = pd.DataFrame({
 merged_df = pd.merge(rune_time_df, met_inst_def, on='timestamp', suffixes = ('_rune', '_met'))
 
 #Beregn forskjellen mellom temperaturene og trykkene
-merged_df['temp_diff'] = merged_df['temprature_rune'] - merged_df['temprature_met']
-merged_df['pres_diff'] = merged_df['presure_rune'] - merged_df['presure_met']
+merged_df['temp_diff'] = (merged_df['temprature_rune'] - merged_df['temprature_met']).abs()
+merged_df['pres_diff'] = (merged_df['presure_rune'] - merged_df['presure_met']).abs()
 
 #Beregn gjennomsnittet av forskjellene
-avg_temp_diff = merged_df['temp_diff'].abs().mean()
-avg_pres_diff = merged_df['pres_diff'].abs().mean()
+avg_temp_diff = merged_df['temp_diff'].mean()
+avg_pres_diff = merged_df['pres_diff'].mean()
 
-#Finn tidspunkt med lavest og høyest forskjell
-lavest_temp_diff_timestamp = merged_df.loc[merged_df['temp_diff'].idxmin()]['timestamp']
-høyest_temp_diff_timestamp = merged_df.loc[merged_df['temp_diff'].idxmax()]['timestamp']
-lavest_pressure_diff_timestamp = merged_df.loc[merged_df['pres_diff'].idxmin()]['timestamp']
-høyest_pressure_diff_timestamp = merged_df.loc[merged_df['pres_diff'].idxmax()]['timestamp']
+'''
+# Finn de to laveste og høyeste temperaturforskjellene
+lavest_temp_diff = merged_df.nsmallest(2, 'temp_diff')
+høyest_temp_diff = merged_df.nlargest(2, 'temp_diff')
 
+# Finn de to laveste og høyeste trykkforskjellene
+lavest_pressure_diff = merged_df.nsmallest(2, 'pres_diff')
+høyest_pressure_diff = merged_df.nlargest(2, 'pres_diff')
+
+'''
+#Finner de to tidspunktene med lavest og høyest temperaturforskjell
+lavest_temp_diff = merged_df.loc[merged_df['temp_diff'].idxmin()]['timestamp']
+høyest_temp_diff = merged_df.loc[merged_df['temp_diff'].idxmax()]['timestamp']
+
+#Finner de to tidspunktene med lavest og høyest trykkforskjell
+lavest_pressure_diff = merged_df.loc[merged_df['pres_diff'].idxmin()]['timestamp']
+høyest_pressure_diff = merged_df.loc[merged_df['pres_diff'].idxmax()]['timestamp']
+
+# Hent råverdiene for de tidspunktene med lavest og høyest temperaturforskjell
+r_lavest_temp_diff = rune_time_df.at[rune_time_df[rune_time_df['timestamp'] == lavest_temp_diff].index[0], 'temprature']
+r_høyest_temp_diff = rune_time_df.at[rune_time_df[rune_time_df['timestamp'] == høyest_temp_diff].index[0], 'temprature']
+
+met_høyest_temp_diff = met_inst_def.at[met_inst_def[met_inst_def['timestamp'] == høyest_temp_diff].index[0], 'temprature']
+met_lavest_temp_diff = met_inst_def.at[met_inst_def[met_inst_def['timestamp'] == lavest_temp_diff].index[0], 'temprature']
+
+r_høyest_pressure_diff = rune_time_df.at[rune_time_df[rune_time_df['timestamp'] == høyest_pressure_diff].index[0], 'presure']
+r_lavest_pressure_diff = rune_time_df.at[rune_time_df[rune_time_df['timestamp'] == lavest_pressure_diff].index[0], 'presure']
+
+met_høyest_pressure_diff = met_inst_def.at[met_inst_def[met_inst_def['timestamp'] == høyest_pressure_diff].index[0], 'presure']
+met_lavest_pressure_diff = met_inst_def.at[met_inst_def[met_inst_def['timestamp'] == lavest_pressure_diff].index[0], 'presure']
 
 if __name__ == "__main__":
-    print("DataFrame: ", merged_df)
-    print("Gjennomsnittlig temperaturforskjell: ", avg_temp_diff)
-    print("Gjennomsnittlig trykkforskjell: ", avg_pres_diff)
-    print("Tidspunkt med lavest temperaturforskjell: ", lavest_temp_diff_timestamp)
-    print("Tidspunkt med høyest temperaturforskjell: ", høyest_temp_diff_timestamp)
-    print("Tidspunkt med lavest trykkforskjell: ", lavest_pressure_diff_timestamp)
-    print("Tidspunkt med høyest trykkforskjell: ", høyest_pressure_diff_timestamp)
+    print("DataFrame:", merged_df)
+    print(f"Gjennomsnittlig temperaturforskjell: { avg_temp_diff:.2f}")
+    print(f"Gjennomsnittlig trykkforskjell: {avg_pres_diff:.2f}")
+    print(f"Tidspunkt med lavest temperaturforskjell: ", lavest_temp_diff, "Rune Temp:", r_lavest_temp_diff, "Met Temp:", met_lavest_temp_diff,
+          f"Temp Diff: {abs(r_lavest_temp_diff - met_lavest_temp_diff):.2f}")
+    print(f"Tidspunkt med høyest temperaturforskjell: ", høyest_temp_diff, "Rune Temp:", r_høyest_temp_diff, "Met Temp:", met_høyest_temp_diff, 
+          f"Temp Diff: {abs(r_høyest_temp_diff - met_høyest_temp_diff):.2f}")
+    print(f"Tidspunkt med lavest trykkforskjell: ", lavest_pressure_diff, "Rune Pressure:", r_lavest_pressure_diff, "Met Pressure:", met_lavest_pressure_diff, 
+          f"Pressure Diff: {abs(r_lavest_pressure_diff - met_lavest_pressure_diff):.2f}")
+    print("Tidspunkt med høyest trykkforskjell: ", høyest_pressure_diff , "Rune Pressure:", r_høyest_pressure_diff, "Met Pressure:", met_høyest_pressure_diff, 
+          f"Pressure Diff: {abs(r_høyest_pressure_diff - met_høyest_pressure_diff):.2f}")
+    
 
+
+'''
+# Hente ut verdien i rad 5, kolonne 'temperature'
+temp_value = df.at[5, 'temperature']
+
+# Hente ut verdien i rad med indeks '2024-11-11 17:00:00', kolonne 'pressure'
+pressure_value = df.at['2024-11-11 17:00:00', 'pressure']
+   '''
--- plotting.py
+++ plotting.py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
-import datetime as d
+import pandas as pd
 
 from sola_time import *
 from rune_time import *
 from samling_av_div import *
+from gjennomsnitt import *
+
+
+#Print må stå her for å printe får grafene kommer opp :)
+print(f"Gjennomsnittlig temperaturforskjell: { avg_temp_diff:.2f}")
+print(f"Gjennomsnittlig trykkforskjell: {avg_pres_diff:.2f}")
+print(f"Tidspunkt med lavest temperaturforskjell: ", lavest_temp_diff, "Rune Temp:", r_lavest_temp_diff, "Met Temp:", met_lavest_temp_diff,
+        f"Temp Diff: {abs(r_lavest_temp_diff - met_lavest_temp_diff):.2f}")
+print(f"Tidspunkt med høyest temperaturforskjell: ", høyest_temp_diff, "Rune Temp:", r_høyest_temp_diff, "Met Temp:", met_høyest_temp_diff, 
+        f"Temp Diff: {abs(r_høyest_temp_diff - met_høyest_temp_diff):.2f}")
+print(f"Tidspunkt med lavest trykkforskjell: ", lavest_pressure_diff, "Rune Pressure:", r_lavest_pressure_diff, "Met Pressure:", met_lavest_pressure_diff, 
+        f"Pressure Diff: {abs(r_lavest_pressure_diff - met_lavest_pressure_diff):.2f}")
+print("Tidspunkt med høyest trykkforskjell: ", høyest_pressure_diff , "Rune Pressure:", r_høyest_pressure_diff, "Met Pressure:", met_høyest_pressure_diff, 
+        f"Pressure Diff: {abs(r_høyest_pressure_diff - met_høyest_pressure_diff):.2f}")
 
 
 plt.subplot(2,1, 1)
@@ -11,15 +25,25 @@ plt.plot(s_dt_dato, s_temp_l, label="Temperatur MET", color="green")
 plt.plot(r_dates_times, r_temps, label="Temperatur", color="blue")
 plt.plot(gyldige_tidspunkter, snitt_temperaturer, label='Gjennomsnittstemperatur', color='orange')
 plt.plot(tid_hoy_lav_x, temp_hoy_lav_y, label='Høyeste og laveste temperatur', color='purple')
+plt.scatter(lavest_temp_diff, r_lavest_temp_diff , marker = "o", color='Red', label='Lavest Temp Diff')
+plt.scatter(lavest_temp_diff, met_lavest_temp_diff, marker = "o", color='Red')
+plt.scatter(høyest_temp_diff, r_høyest_temp_diff, marker = "x", color='Red', label='Høyest Temp Diff')
+plt.scatter(høyest_temp_diff, met_høyest_temp_diff, marker = "x", color='Red')
 plt.legend()
-
+plt.grid(True)
 
 plt.subplot(2, 1, 2)
+plt.scatter(lavest_pressure_diff, r_lavest_pressure_diff, marker="o", color='Red', label='Lavest Pressure Diff')
+plt.scatter(lavest_pressure_diff, met_lavest_pressure_diff, marker="o", color='Red')
+plt.scatter(høyest_pressure_diff, r_høyest_pressure_diff, marker="x", color='Red', label='Høyest Pressure Diff')
+plt.scatter(høyest_pressure_diff, met_høyest_pressure_diff, marker="x", color='Red')
 plt.plot(s_dt_dato, s_trykk_l, label="Absolutt trykk MET", color="green")
 plt.plot(r_dates_times, r_trykk_a, label="Absolutt trykk", color="blue")
 plt.plot(r_dates_times, r_trykk_b, label="Barometrisk trykk", color="orange")
 plt.legend()
-
+plt.grid(True)
 
 
 plt.show()
+
+
